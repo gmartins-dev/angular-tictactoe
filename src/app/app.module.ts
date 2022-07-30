0a6e76c45c500c5c6dfbd8bb24490e798b807294
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 11:55:59 +0100

    criada parte logica do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { GameComponent } from './components/game/game.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    GameComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
