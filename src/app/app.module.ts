ac7ba43625cbc72306608edc268d832cb92cc07a
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 11:58:40 +0100

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { GameComponent } from './components/game/game.component';
 import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ComponentsComponent,
-    GameComponent,
-    BoardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GameComponent, BoardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
