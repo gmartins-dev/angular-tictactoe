a009cbc5daad65b3f4482a083eaef7857556408d
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 12:37:58 +0100

    game funcionando

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -31,7 +31,7 @@ export class GameComponent implements OnInit {
 
   makeMove(idx: number) {
     if (!this.squares[idx]) {
-      this.squares.splice(idx, 1, this.xIsNext);
+      this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
       this.counter++;
     }
